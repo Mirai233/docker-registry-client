a08fe4b27937349fc961a513e9674a67f6d51617
mirai233 <1982452674@qq.com>
2025-03-04 16:33:20 +0800

    fix(registry): improve handling of relative URLs in link headers
    - Update module path to github.com/Mirai233/docker-registry-client
    - Enhance getNextLink function to handle both absolute and relative URLs
    - Add error handling for cases where URL parsing fails

--- registry/json.go
+++ registry/json.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"net/http"
 	"regexp"
+	"strings"
 )
 
 var (
@@ -26,13 +27,23 @@ func (registry *Registry) getPaginatedJSON(url string, response interface{}) (st
 	if err != nil {
 		return "", err
 	}
-	return getNextLink(resp)
+
+	url, err = getNextLink(resp)
+	if err != nil {
+		return "", err
+	}
+
+	if strings.Contains(url, "://") {
+		return url, nil
+	}
+
+	return registry.url(url), nil
 }
 
 // Matches an RFC 5988 (https://tools.ietf.org/html/rfc5988#section-5)
 // Link header. For example,
 //
-//    <http://registry.example.com/v2/_catalog?n=5&last=tag5>; type="application/json"; rel="next"
+//	<http://registry.example.com/v2/_catalog?n=5&last=tag5>; type="application/json"; rel="next"
 //
 // The URL is _supposed_ to be wrapped by angle brackets `< ... >`,
 // but e.g., quay.io does not include them. Similarly, params like
