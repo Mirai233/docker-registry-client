036e9f97ff37ad25a54ec6656ab81665110de48c
mirai233 <1982452674@qq.com>
2025-03-21 10:04:11 +0800

    feat(registry): add support for manifest lists
    - Implement ManifestList struct and related functions
    - Add ManifestEntry and PlatformSpec structs
    - Implement ManifestList method to fetch and parse manifest lists- Add Arch and OS methods to extract platform information from manifest lists

--- registry/manifest.go
+++ registry/manifest.go
@@ -2,6 +2,8 @@ package registry
 
 import (
 	"bytes"
+	"encoding/json"
+	"io"
 	"io/ioutil"
 	"net/http"
 
@@ -10,6 +12,26 @@ import (
 	"github.com/docker/distribution/manifest/schema2"
 	digest "github.com/opencontainers/go-digest"
 )
+
+type ManifestList struct {
+	MediaType     string          `json:"mediaType"`
+	SchemaVersion int             `json:"schemaVersion"`
+	Manifests     []ManifestEntry `json:"manifests"`
+}
+
+type ManifestEntry struct {
+	MediaType string       `json:"mediaType"`
+	Digest    string       `json:"digest"`
+	Size      int          `json:"size"`
+	Platform  PlatformSpec `json:"platform"`
+}
+
+type PlatformSpec struct {
+	Architecture string `json:"architecture"`
+	OS           string `json:"os"`
+}
+
+const manifestListMediaType = "application/vnd.docker.distribution.manifest.list.v2+json"
 
 func (registry *Registry) Manifest(repository, reference string) (*schema1.SignedManifest, error) {
 	url := registry.url("/v2/%s/manifests/%s", repository, reference)
@@ -86,10 +108,40 @@ func (registry *Registry) ManifestDigest(repository, reference string) (digest.D
 	}
 	defer resp.Body.Close()
 
-	if err != nil {
-		return "", err
-	}
 	return digest.Parse(resp.Header.Get("Docker-Content-Digest"))
+}
+
+func (registry *Registry) ManifestList(repository, reference string) (bool, *ManifestList, error) {
+	url := registry.url("/v2/%s/manifests/%s", repository, reference)
+	registry.Logf("registry.manifest.head url=%s repository=%s reference=%s", url, repository, reference)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return false, nil, err
+	}
+
+	req.Header.Set("Accept", manifestListMediaType)
+	resp, err := registry.Client.Do(req)
+	if err != nil {
+		return false, nil, err
+	}
+	defer resp.Body.Close()
+
+	manifestBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return false, nil, err
+	}
+
+	var manifestList ManifestList
+	if err = json.Unmarshal(manifestBytes, &manifestList); err != nil {
+		return false, nil, err
+	}
+
+	isManifestList := manifestList.MediaType == manifestListMediaType &&
+		manifestList.SchemaVersion == 2 &&
+		len(manifestList.Manifests) > 0
+
+	return isManifestList, &manifestList, nil
 }
 
 func (registry *Registry) DeleteManifest(repository string, digest digest.Digest) error {
@@ -131,4 +183,20 @@ func (registry *Registry) PutManifest(repository, reference string, manifest dis
 		defer resp.Body.Close()
 	}
 	return err
+}
+
+func (m *ManifestList) Arch() []string {
+	arch := make([]string, 0)
+	for _, manifest := range m.Manifests {
+		arch = append(arch, manifest.Platform.Architecture)
+	}
+	return arch
+}
+
+func (m *ManifestList) OS() []string {
+	os := make([]string, 0)
+	for _, manifest := range m.Manifests {
+		os = append(os, manifest.Platform.OS)
+	}
+	return os
 }
